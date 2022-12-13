49c2c92efc8e975b07b34377eb9f8c92fd6537e9
ChaosNuggets <46007806+ChaosNuggets@users.noreply.github.com>
2022-12-13 11:14:49 -0500

    finished the dumb website (unless I want to actually save the data to a form)

--- detect_location.js
+++ detect_location.js
@@ -1,16 +1,16 @@
-
-/* Add "https://api.ipify.org?format=json" statement
-this will communicate with the ipify servers in
-order to retrieve the IP address $.getJSON will
-load JSON-encoded data from the server using a
-GET HTTP request */
-
-$.getJSON("https://api.ipify.org?format=json", function(data) {
+// Get the IPv6
+$.getJSON("https://api6.ipify.org?format=json", function(data) {
 
   // Setting text of element P with id gfg
-  document.getElementById(`ipAddress`).innerHTML = data.ip;
+  document.getElementById(`ipv6`).innerHTML = data.ip;
 })
 
+// Get the IPv4
+$.getJSON("https://api4.ipify.org?format=json", function(data) {
+
+  // Setting text of element P with id gfg
+  document.getElementById(`ipv4`).innerHTML = data.ip;
+})
 
 
 if ("geolocation" in navigator) {
@@ -25,11 +25,10 @@ if ("geolocation" in navigator) {
 
       function error(error_message) {
         // for when getting location results in an error
-        console.error('An error has occured while retrieving' +
-                  'location', error_message)
+        console.log(error_message);
   });
 } else {
   // geolocation is not supported
   // get your location some other way
-  console.log('geolocation is not enabled on this browser')
+  console.log('geolocation is not enabled on this browser');
 }
